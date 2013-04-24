9d7ef0572aec3edec8f2309c06c22ece399da96f
Tyler Davis <tyebud@gmail.com>
2013-04-24 13:30:14 -0400

    Added local forever dep

---
+++ Gemfile
@@ -0,0 +1,2 @@
+source "https://rubygems.org"
+gem 'capper', :git => "git@github.com:adeven/capper.git"
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,25 @@
 set :application, "TextBob"
 set :repository,  "git@github.com:tylerdavis/text-tweet-bot.git"
 
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
 set :scm, :git
 
-role :web, "nginx"                          # Your HTTP server, Apache/etc
-role :app, "node"                          # This may be the same as your `Web` server
+set :use_sudo, false
+set :keep_releases, 5
+set :deploy_via, :remote_cache
+set :main_js, 'app.js'
+
+set :symlinks, {"log" => "log"}
+
+set :branch, 'master'
+set :user, 'ubuntu'
+set :deploy_to, "/home/#{user}/deploy/#{application}"
+server "ec2-54-224-141-226.compute-1.amazonaws.com", :app
+set :forever_cmd, "/usr/bin/forever"
+
+desc "Tail the application logfile"
+task :log do
+  log ="#{application_dir}/current/log/#{application}.log"
+  run "tail -f #{log}"
+end
